b6f837607050b9f912d4401af8985e034e01c35e
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2019-05-31 13:46:08 -0400

    feat: new generic asset/unit abstraction
    - no assets/units are defined, consumers must define them
    - provide utils for converting between units/scales: account, base, exchange
    - create separate types for units (asset + unit) and quantities (asset + unit + amount) for better type checking
    - less opinionated
    - fix vulnerabilities and update deps

--- README.md
+++ README.md
@@ -1,26 +1,9 @@
 # crypto-rate-utils
 
-> Fetch exchange rates for conversions between crypto assets
+> Fetch exchange rates and convert between units of crypto assets
 
 ### Install
 
 ```shell
 npm i @kava-labs/crypto-rate-utils
-```
-
-### Usage
-
-```js
-import { connectCoinCap, eth, gwei, satoshi, convert } from 'crypto-rate-utils'
-
-async function run() {
-  const api = await connectCoinCap()
-
-  convert(gwei(1200000), eth()) // => BN(0.0012)
-  
-  // API is required for cross-currency conversions
-  convert(gwei(1200000), satoshi(), api) // => BN(...)
-}
-
-run().catch(err => console.error(err))
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,133 @@
-import { connectCoinCap } from './api/coincap'
 import BigNumber from 'bignumber.js'
 
-interface RateApi {
+export { connectCoinCap } from './api/coincap'
+
+export interface RateApi {
+  /** Fetch the price of the asset denoted by the given symbol in US dollars */
   getPrice: (symbol: string) => BigNumber
+
+  /** Gracefully disconnect the connection to the API provider */
   disconnect: () => Promise<void>
 }
 
+export interface AssetUnit {
+  /** Unique identifier for the asset (typically a 3 or 4 character uppercase code) */
+  readonly symbol: string
+
+  /** Orders of magnitude between the unit of exchange and the base unit */
+  readonly exchangeScale: number
+
+  /** Orders of magnitude between the unit of account and the base unit */
+  readonly accountScale: number
+
+  /**
+   * Number of orders of magnitude between this unit and the base unit
+   * - Defines the unit of the asset (e.g. ether, gwei, or wei, in the case of the asset ETH)
+   * - By default, this should be 0, the base unit (typically the smallest denomination of the asset)
+   */
+  readonly scale: number
+}
+
+export interface AssetQuantity extends AssetUnit {
+  /** Quantity of the asset */
+  readonly amount: BigNumber
+}
+
+/** Create the base unit for the given asset */
+export const baseUnit = (unit: AssetUnit): AssetUnit => ({
+  ...unit,
+  scale: 0
+})
+
+/** Create the unit of exchange for the given asset */
+export const exchangeUnit = (unit: AssetUnit): AssetUnit => ({
+  ...unit,
+  scale: unit.exchangeScale
+})
+
+/** Create the unit of account for the given asset */
+export const accountUnit = (unit: AssetUnit): AssetUnit => ({
+  ...unit,
+  scale: unit.accountScale
+})
+
 /**
- * Unique 3 or 4 character code for the given asset
+ * Create or convert a quantity to a different unit of the given asset
+ * - If given a quantity, convert it to the new unit
+ * - If given an asset and amount, create a quantity with the amount
  */
-enum AssetCode {
-  Btc = 'BTC',
-  Eth = 'ETH',
-  Xrp = 'XRP',
-  Usd = 'USD'
+export interface ConvertQuantity {
+  (unit: AssetUnit, amount: BigNumber.Value): AssetQuantity
+  (unit: AssetQuantity): AssetQuantity
 }
 
-interface AssetUnit {
-  /**
-   * Difference in orders of magnitude between this unit and this asset's
-   * base unit, or its smallest denomination on ledger
-   */
-  unit: number
-  /** Unique 3 or 4 digit character code for the given asset (e.g. "BTC") */
-  symbol: AssetCode
-  /** Amount of the asset (positive), or 1 to functionally get the rate */
-  amount: BigNumber
-  /**
-   * Scale of the unit of exchange, relative to the base unit of the ledger
-   * (e.g. Bitcoin, the unit of exchange, is 8 orders or magnitude larger
-   * than a satoshi, the smallest denomination)
-   */
-  exchangeUnit: number
-}
-
-type CreateAssetUnit = (unit: number) => (amount?: BigNumber.Value) => AssetUnit
-
-const ethAsset: CreateAssetUnit = unit => amount => ({
-  unit,
-  exchangeUnit: 18,
-  pluginBase: 9,
-  symbol: AssetCode.Eth,
-  amount: new BigNumber(amount || 1)
+/**
+ * Create or convert a quantity to the base unit (typically the smallest denomination) of the given asset
+ * - If given a quantity, convert it to the base unit
+ * - If given an amount, create a quantity with that amount
+ */
+export const baseQuantity: ConvertQuantity = (
+  unit: AssetUnit | AssetQuantity,
+  amount?: BigNumber.Value
+) => ({
+  ...unit,
+  scale: 0,
+  amount:
+    'amount' in unit
+      ? unit.amount.shiftedBy(unit.scale)
+      : new BigNumber(amount!)
 })
 
-const eth = ethAsset(18)
-const gwei = ethAsset(9)
-const wei = ethAsset(0)
-
-const xrpAsset: CreateAssetUnit = unit => amount => ({
-  unit,
-  exchangeUnit: 6,
-  symbol: AssetCode.Xrp,
-  amount: new BigNumber(amount || 1)
+/**
+ * Create or convert a quantity to the unit of exchange of the given asset
+ * - If given a quantity, convert it to the unit of exchange
+ * - If given an asset and amount, create a quantity with that amount
+ */
+export const exchangeQuantity: ConvertQuantity = (
+  unit: AssetUnit | AssetQuantity,
+  amount?: BigNumber.Value
+) => ({
+  ...unit,
+  scale: unit.exchangeScale,
+  amount:
+    'amount' in unit
+      ? unit.amount.shiftedBy(unit.scale - unit.exchangeScale)
+      : new BigNumber(amount!)
 })
 
-const xrp = xrpAsset(6)
-const drop = xrpAsset(0)
-const xrpBase = xrpAsset(-3)
-
-const btcAsset: CreateAssetUnit = unit => amount => ({
-  unit,
-  exchangeUnit: 8,
-  symbol: AssetCode.Btc,
-  amount: new BigNumber(amount || 1)
+/**
+ * Create or convert a quantity to the unit of account of the given asset
+ * - If given a quantity, convert it to the unit of account
+ * - If given an asset and amount, create a quantity with that amount
+ */
+export const accountQuantity: ConvertQuantity = (
+  unit: AssetUnit | AssetQuantity,
+  amount?: BigNumber.Value
+) => ({
+  ...unit,
+  scale: unit.accountScale,
+  amount:
+    'amount' in unit
+      ? unit.amount.shiftedBy(unit.scale - unit.accountScale)
+      : new BigNumber(amount!)
 })
 
-const btc = btcAsset(8)
-const satoshi = btcAsset(0)
-
-const usdAsset: CreateAssetUnit = unit => amount => ({
-  unit,
-  exchangeUnit: 2,
-  symbol: AssetCode.Usd,
-  amount: new BigNumber(amount || 1)
+/**
+ * Determine quantity of destination asset for the given amount of the source asset
+ * - If no amount is provided for the source unit, returns the exchange rate
+ * - Backend must be provided to convert between different types of assets
+ * @param source Source asset, its amount, and its unit
+ * @param dest Destination asset and its unit (amount disregarded)
+ * @param apiOrRate Backend to fetch prices, or an exchange rate
+ */
+export const convert = (
+  source: AssetQuantity,
+  dest: AssetUnit,
+  api?: RateApi
+): AssetQuantity => ({
+  ...dest,
+  amount: source.amount.times(getRate(source, dest, api))
 })
-
-const usd = usdAsset(2)
 
 /**
  * Determine quantity of destination asset given 1 unit of source asset, in given units
@@ -107,49 +158,6 @@ const getRate = (
   // Since the rate is in the unit of exchange (e.g. BTC, ETH),
   // it must be converted to scale of the given unit
   return rate.shiftedBy(
-    source.unit - source.exchangeUnit - (dest.unit - dest.exchangeUnit)
+    source.scale - source.exchangeScale - (dest.scale - dest.exchangeScale)
   )
-}
-
-/**
- * Determine quantity of destination asset for the given amount of the source asset
- * - If no amount is provided for the source unit, returns the exchange rate
- * - Backend must be provided to convert between different types of assets
- * @param source Source assest, its amount, and its unit
- * @param dest Destination asset and its unit (amount disregarded)
- * @param apiOrRate Backend to fetch prices, or an exchange rate
- */
-const convert = (
-  source: AssetUnit,
-  dest: AssetUnit,
-  apiOrRate?: RateApi | BigNumber
-): BigNumber => {
-  const isBigNumber = (o: any): o is BigNumber => BigNumber.isBigNumber(o)
-  const rate = isBigNumber(apiOrRate)
-    ? apiOrRate
-    : getRate(source, dest, apiOrRate)
-
-  return source.amount.times(rate)
-}
-
-export {
-  // Rate backend
-  connectCoinCap,
-  RateApi,
-  // Utilities for rates and converting units
-  convert,
-  AssetUnit,
-  // Creating BTC units
-  btc,
-  satoshi,
-  // Creating ETH units
-  eth,
-  gwei,
-  wei,
-  // Creating XRP units
-  xrp,
-  drop,
-  xrpBase,
-  // Creating USD units
-  usd
 }
